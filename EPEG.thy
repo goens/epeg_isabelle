896024e2ec45367b9ba7cff86443c81a082903e4
Andrés Goens <andres@goens.org>
2024-02-21 21:18:05 +0100

    Add a WIP definition of step

--- EPEG.thy
+++ EPEG.thy
@@ -19,6 +19,10 @@ datatype expr =
   Nu nonterm |
   Gamma nonterm 
 
+fun All :: "expr list \<Rightarrow> expr" where
+  "All Nil = Empty" |
+  "All (x#xs) = foldl Seq x xs" 
+
 record EPEG =
   production :: "(nonterm \<times> expr) list"
   table :: "(nonterm \<times> (symbol list)) list" 
@@ -85,7 +89,7 @@ where
   Not_0: "hook \<Gamma> e Fail \<Longrightarrow> hook \<Gamma> (Not e) Succ0" |
   Not_f: "succeeds \<Gamma> e \<Longrightarrow> hook \<Gamma> (Not e) Fail" |
   Seq_0: "hook \<Gamma> e1 Succ0 \<Longrightarrow> hook \<Gamma> e2 Succ0 \<Longrightarrow> hook \<Gamma> (Seq e1 e2) Succ0" |
-  Seq_1_first: "succeeds \<Gamma> e Succ1 \<Longrightarrow> hook \<Gamma> e2 Succ1 \<Longrightarrow> hook \<Gamma> (Seq eSucc1 e2) Succ1" |
+  Seq_1_first: "succeeds \<Gamma> e \<Longrightarrow> hook \<Gamma> e2 Succ1 \<Longrightarrow> hook \<Gamma> (Seq eSucc1 e2) Succ1" |
   Seq_1_second: "hook \<Gamma> e1 Succ1 \<Longrightarrow> succeeds \<Gamma> e2 \<Longrightarrow> hook \<Gamma> (Seq e1 e2) Succ1" |
   Seq_f_first: "hook \<Gamma> e1 Fail \<Longrightarrow> hook \<Gamma> (Seq e1 e2) Fail" |
   Seq_f_second: "succeeds \<Gamma> e1 \<Longrightarrow> hook \<Gamma> e2 Fail \<Longrightarrow> hook \<Gamma> (Seq e1 e2) Fail" |
@@ -104,5 +108,43 @@ where
 
 code_pred hook.
 code_pred succeeds.
+
+inductive step :: "expr \<Rightarrow> char list \<Rightarrow> EPEG \<Rightarrow> string option \<Rightarrow> (nonterm \<times> expr) list \<Rightarrow> bool" where
+  Term_s: "step (Term a) (a # x) \<Gamma> (Some [a]) (production \<Gamma>)" |
+  Term_f_neq: "a \<noteq> b \<Longrightarrow> step (Term a) (b # x) \<Gamma> None (production \<Gamma>)" |
+  Term_f_empty: "step (Term a) [] \<Gamma> None (production \<Gamma>)" |
+  Nonterm: "lookup \<Gamma> A = e \<Longrightarrow> step e x \<Gamma> out R \<Longrightarrow> step (Nonterm A) x \<Gamma> out R" |
+  Empty: "step Empty x \<Gamma> (Some []) (production \<Gamma>)" |
+  Seq_s: "step e1 (x1 @ x2 @ y) \<Gamma> (Some x1) R1 \<Longrightarrow>
+          step e2 (x2 @ y) (\<Gamma>\<lparr>production := R1\<rparr>) (Some x2) R2 \<Longrightarrow>
+          step (Seq e1 e2) (x1 @ x2 @ y) \<Gamma> (Some (x1 @ x2)) R2" |
+  Seq_f_fst: "step e1 x \<Gamma> None R \<Longrightarrow> step (Seq e1 e2) x \<Gamma> None (production \<Gamma>)" |
+  Seq_f_snd: "step e1 (x @ y) \<Gamma> (Some x) R1 \<Longrightarrow>
+              step e2 y (\<Gamma>\<lparr>production := R1\<rparr>) None R2 \<Longrightarrow>
+              step (Seq e1 e2) (x @ y) \<Gamma> None (production \<Gamma>)" |
+  Choice_s_fst: "step e1 (x @ y) \<Gamma> (Some x) R \<Longrightarrow> step (Choice e1 e2) (x @ y) \<Gamma> (Some x) R" |
+  Choice_s_snd: "step e1 (x @ y) \<Gamma> None R1 \<Longrightarrow>
+                 step e2 (x @ y) \<Gamma> (Some x) R2 \<Longrightarrow>
+                 step (Choice e1 e2) (x @ y) \<Gamma> (Some x) R2" |
+  Choice_f: "step e1 x \<Gamma> None R1 \<Longrightarrow>
+             step e2 x \<Gamma> None R2 \<Longrightarrow>
+             step (Choice e1 e2) x \<Gamma> None (production \<Gamma>)" |
+  Not_s: "step e x \<Gamma> None R \<Longrightarrow> step (Not e) x \<Gamma> (Some []) (production \<Gamma>)" |
+  Not_f: "step e (x @ y) \<Gamma> (Some x) R \<Longrightarrow> step (Not e) (x @ y) \<Gamma> None (production \<Gamma>)" |
+  Star_base: "step e x \<Gamma> None R \<Longrightarrow> step (Star e) x \<Gamma> (Some []) (production \<Gamma>)" |
+  Star_ind: "step e (x1 @ x2 @ y) \<Gamma> (Some x1) R1 \<Longrightarrow>
+             step (Star e) (x2 @ y) (\<Gamma>\<lparr>production := R1\<rparr>) (Some x2) R2 \<Longrightarrow>
+             step (Star e) (x1 @ x2 @ y) \<Gamma> (Some (x1 @ x2)) R2" |
+  (*Bind_s: "step e (x @ y) \<Gamma> (Some x) R1 \<Longrightarrow>
+           step (Delta e i) (x @ y) \<Gamma> (Some x) (s, expr.Choice (All (map x expr.Term)) (lookup R1 i))#R1" |*)
+  Bind_f: "s step e x \<Gamma> None R \<Longrightarrow> step (Delta e i) x \<Gamma> None (production \<Gamma>)" |
+  (*
+   Mod_s: "step e (x@y) \<Gamma> (Some x) R₁
+            (* u is the original update list, and v is the one with \<gamma> and \<nu> removed.*)
+            (* still not entirely translated from Lean: \<Longrightarrow> \<forall> uv \<in> u.zip v, (uv.fst.fst = uv.snd.fst \<and> elimRefs uv.fst.snd = uv.fst.snd) *)
+            \<Longrightarrow> step (Mu e u) (x@y) \<Gamma> (Some x) (v@R)
+   *)
+  Mod_f : "step e x \<Gamma> None R \<Longrightarrow> step (mu e p) x \<Gamma> None (production \<Gamma>)"
+
 
 end
